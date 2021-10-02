db3a3e0fd892106b889c578cd70e424aade569a8
dohagtae <willd88@naver.com>
2021-10-02 21:46:24 +0900

    2021-10-2 비동기 문제 해결 일부 버그문제 해결 time쪽 작업중

--- app/src/main/java/com/will_d/yogadesign/time/TimeTimerFragment.java
+++ app/src/main/java/com/will_d/yogadesign/time/TimeTimerFragment.java
@@ -1,28 +1,39 @@
 package com.will_d.yogadesign.time;
 
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.will_d.yogadesign.R;
+
+import java.util.ArrayList;
 
 import soup.neumorphism.NeumorphCardView;
 
 public class TimeTimerFragment extends Fragment {
-    private TextView tvHour;
-    private TextView tvMinutes;
-    private TextView tvSeconds;
+    private TextView tvTime;
 
 
     private TextView tvInit;
     private NeumorphCardView ncdPlayAndPuase;
+    private ImageView ivPlayAndPuase;
+    private TextView tvSave;
 
+    private RelativeLayout rlClickSaveDialog;
+    private RecyclerView recyclerView;
+
+    private TimeThread timeThread;
+    private boolean isPlayAndPuase = false;
 
     @Nullable
     @Override
@@ -34,16 +45,135 @@ public class TimeTimerFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        tvHour = view.findViewById(R.id.tv_hour);
-        tvMinutes = view.findViewById(R.id.tv_minutes);
-        tvSeconds = view.findViewById(R.id.tv_seconds);
+        tvTime = view.findViewById(R.id.tv_time);
+
 
         tvInit = view.findViewById(R.id.tv_init);
         ncdPlayAndPuase = view.findViewById(R.id.ncd_playAndPuase);
+        ivPlayAndPuase = view.findViewById(R.id.iv_playAndPuase);
+        tvSave = view.findViewById(R.id.tv_save);
+
+        rlClickSaveDialog = view.findViewById(R.id.rl_clicksave_dialog);
+        recyclerView = view.findViewById(R.id.recycler);
 
 
+        ncdPlayAndPuase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //true면 play false면 pause
+                isPlayAndPuase = !isPlayAndPuase;
+                if (isPlayAndPuase){
+                    ivPlayAndPuase.setImageResource(R.drawable.ic_pause);
+                    if (timeThread==null){
+                        timeThread = new TimeThread();
+                        timeThread.start();
+                    }else {
+                        timeThread.resumeThread();
+                    }
 
+                }else {
+                    ivPlayAndPuase.setImageResource(R.drawable.ic_play);
+                    if (timeThread !=null) timeThread.pauseThread();
+                }
+
+            }
+        });
+
+        tvInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (timeThread!=null){
+                    ivPlayAndPuase.setImageResource(R.drawable.ic_play);
+                    timeThread.stopThread();
+                    timeThread.time=0;
+                    timeThread.min=0;
+                    timeThread.sec=0;
+                    tvTime.setText("00:00:00");
+                    timeThread = null;
+                }
+            }
+        });
+
+        tvSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
     }
+
+    class TimeThread extends Thread {
+        boolean isRun = true;
+        boolean isWait = false;
+
+        int time, min, sec;
+
+        @Override
+        public void run() {
+
+            while (isRun) {
+                sec++;
+                if (sec >= 60) {
+                    sec = 0;
+                    min++;
+                    if (min >= 60) {
+                        min = 0;
+                        time++;
+                        if (time>=24){
+                            time=0;
+                        }
+                    }
+                }
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String s = String.format("%02d:%02d:%02d", time, min, sec);
+                        tvTime.setText(s);
+                    }
+                });
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (this){
+                    if (isWait) {
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+
+            }
+        }//run method...
+
+        void stopThread(){
+            isRun = false;
+
+            synchronized (this){
+                this.notify();
+            }
+        }
+
+        void pauseThread(){
+            isWait =true;
+        }
+
+        void resumeThread(){
+            isWait =false;
+
+            synchronized (this){
+                this.notify();
+            }
+        }
+
+    }//TimeThread inner class.....
 }
---
+++ app/src/main/java/com/will_d/yogadesign/time/TimerItem.java
@@ -0,0 +1,15 @@
+package com.will_d.yogadesign.time;
+
+public class TimerItem {
+    private String nickName;
+
+    public TimerItem(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+}
+
+
